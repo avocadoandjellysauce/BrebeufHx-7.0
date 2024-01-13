79454d47e7c75fbcb19023caf947d98cf37e1474
avocadoandjellysauce <121403486+avocadoandjellysauce@users.noreply.github.com>
2024-01-12 20:18:38 -0500

    Template landing page

--- brebeufhx-7.0/src/App.js
+++ brebeufhx-7.0/src/App.js
@@ -1,14 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Home from "./Components/Home";
+import About from "./Components/About";
+import Work from "./Components/Work";
+import Testimonial from "./Components/Testimonial";
+import Contact from "./Components/Contact";
+import Footer from "./Components/Footer";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-        
-        </a>
-      </header>
+      <Home />
+      <About />
+      <Work />
+      <Testimonial />
+      <Contact />
+      <Footer />
     </div>
   );
 }
