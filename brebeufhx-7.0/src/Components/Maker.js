2b42870ce8be47b5191561af8ef8dad47a9a5616
avocadoandjellysauce <121403486+avocadoandjellysauce@users.noreply.github.com>
2024-01-13 11:31:45 -0500

    maker2

--- brebeufhx-7.0/src/Components/Maker.js
+++ brebeufhx-7.0/src/Components/Maker.js
@@ -1,8 +1,24 @@
 import React from "react";
 
-
-
 const Maker = () => {
+  const [value, setInputValue] = React.useState('');
+  const [experience1, setExperience1] = React.useState('');
+  const [experience2, setExperience2] = React.useState('');
+  const [experience3, setExperience3] = React.useState('');
+
+  const handleInputChange = (setter) => (event) => {
+    setter(event.target.value);
+  };
+  const TextGenerate = (value) => {
+    console.log('Generating Text...',{ value });
+    // You can use experience1, experience2, experience3 as needed
+  };
+
+  const handleFormSubmit = () => {
+    // Call a function or perform actions with the experience values
+    TextGenerate({value});
+  };
+
   return (
     <div className="contact-page-wrapper">
       <h1 className="primary-heading">CV Maker</h1>
@@ -30,20 +46,22 @@ const Maker = () => {
         
       </div>
       <div className="experience-form-container">
-        <input type="text" placeholder="Job Experience 1" />
-        <button className="secondary-button">Submit</button>
+        <input type="text" value={experience1} onChange={handleInputChange(setExperience1)} placeholder="Job Experience 1" />
+        <button className="secondary-button" onClick={handleFormSubmit}>Submit</button>
       </div>
       <div className="experience-form-container">
-        <input type="text" placeholder="Job Experience 2" />
-        <button className="secondary-button">Submit</button>
+      <input type="text" value={experience2} onChange={handleInputChange(setExperience2)} placeholder="Job Experience 2" />
+        <button className="secondary-button" onClick={handleFormSubmit}>Submit</button>
       </div>
       
       <div className="experience-form-container">
-        <input type="text" placeholder="Job Experience 3" />
-        <button className="secondary-button">Submit</button>
+        <input type="text" value={experience3} onChange={handleInputChange(setExperience3)} placeholder="Job Experience 3" />
+        <button className="secondary-button" onClick={handleFormSubmit}>Submit</button>
       </div>
+      
     </div>
   );
 };
 
 export default Maker;
+
